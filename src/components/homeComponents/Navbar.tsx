a83168df44d7019cea82434e89fd1273c3718fc6
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-09-16 22:50:27 +0900

    style: navbar is fixed.

--- src/components/homeComponents/Navbar.tsx
+++ src/components/homeComponents/Navbar.tsx
@@ -1,107 +1,105 @@
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import { useTranslation } from 'react-i18next';
 import Langs from '../../interfaces/LayoutType';
 
-
-
 const langs: any = {
-  en: { nativeName: 'English'},
-  jp: { nativeName: '日本語'}, 
-  it: { nativeName: 'Italiano'},
-  de: { nativeName: 'Deutsch'},
-  pt: { nativeName: 'Português'},
+  en: { nativeName: 'English', flag: "/flagassets/us.svg" },
+  jp: { nativeName: '日本語', flag: "/flagassets/jp.svg"}, 
+  it: { nativeName: 'Italiano', flag: "/flagassets/it.svg"},
+  de: { nativeName: 'Deutsch', flag: "/flagassets/de.svg"},
+  pt: { nativeName: 'Português', flag: "/flagassets/br.svg"},
 };
 
 interface Navigation  { 
   name: any;
   href: string;
-  current: boolean;
   key: string;
-};
+}
 
 const Navbar = ({ setCurrentLang, currentLang }: Langs) => {
   const { t, i18n } = useTranslation();
-  
+
   const handleLangChange = (lang: string) => {
     i18n.changeLanguage(lang);
     setCurrentLang(lang);
     sessionStorage.setItem("lang", lang);
-    console.log(sessionStorage.getItem("lang"));
   }
-  
+
   const navigation: Navigation[] = [
-    { name: t("navbar.blog"), href: "/blogs", current: false, key: "blog"},
-    { name: t("navbar.join"), href: "/volunteers", current: false, key: "join"},
-    { name: t("navbar.contact"), href: '/contactus', current: false, key: "contact" },
-    { name: t("navbar.about"), href: '/aboutus', current: false, key: "about" },
-    { name: t("navbar.game"), href: "/pridefarmgame", current: false, key: "game" },
+    { name: t("navbar.blog"), href: "/blogs", key: "blog" },
+    { name: t("navbar.join"), href: "/volunteers", key: "join" },
+    { name: t("navbar.contact"), href: '/contactus', key: "contact" },
+    { name: t("navbar.about"), href: '/aboutus', key: "about" },
+    { name: t("navbar.game"), href: "/pridefarmgame", key: "game" },
   ];
-  
-  
+
   return (
-    <Disclosure as="nav" className="bg-gradient-to-br from-green-500 via-green-600 to-green-600 shadow-2xl sticky top-0 z-50">
-      <div className="px-6 md:px-2">
-        <div className="relative flex h-24 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-            <DisclosureButton className="inline-flex items-center justify-center p-2 text-white hover:bg-green-700 
-            focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white transition-transform transform hover:scale-105">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true"className="h-8 w-8" />
-              <XMarkIcon aria-hidden="true"className="h-8 w-8" />
-            </DisclosureButton>
-          </div>
-              <a href="./">
-              <img
-                alt="プライドランドロゴ"
-                src='/logo.png'
-                className="h-24 p-2 m-0"
-              />
-              </a>
-              <div className='hidden md:flex md:items-center md:space-x-4'>
-              </div>
-          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-end">
-            <div className="hidden md:ml-6 md:block">
-              <div className="flex">
-                {navigation.map((nav) => <a key={nav.key} href={nav.href}  className=
-                      ' px-4 py-6 text-lg font-medium rounded-lg gap-2  transition-colors duration-300 hover:bg-green-800  hover:text-yellow-200 text-white text-md'>{nav.name}</a> )}
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-
-<select defaultValue={currentLang} onChange={(e) => handleLangChange(e.target.value)} className="bg-white dark:bg-gray-700 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-600 rounded-lg px-4 py-2">
-  <span className="ms-4 ">{langs[currentLang].nativeName} </span>
-    // Dropdown items mapped from langs object
-       {Object.keys(langs).map((lang) => (
-    <option key={lang} value={lang}>
-        <span className="ms-4">{langs[lang].nativeName}</span>
-      </option>
-    ))}
-</select>
-          </div>
-        </div>
-      </div>
-
-      <DisclosurePanel className="md:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
-              className=
-                'bg-green-700 text-white block rounded-md px-3 py-2 text-lg font-medium'
-            > 
-              {item.name}
-            </DisclosureButton>
+    <nav className="bg-gradient-to-br from-green-500 via-green-600 to-green-600 shadow-2xl md:sticky top-0 z-50">
+      <div className="flex h-24 items-center justify-between px-4">
+        {/* Logo */}
+        <a href="./">
+          <img
+            alt="プライドランドロゴ"
+            src='/logo.png'
+            className="h-24 p-2 m-0"
+          />
+        </a>
+        {/* desktop view links */}
+        <div className="hidden md:flex space-x-4">
+          {navigation.map((nav) => (
+            <a
+              key={nav.key}
+              href={nav.href}
+              className="px-4 py-6 text-lg font-medium rounded-lg text-white hover:bg-green-800 hover:text-yellow-200"
+            >
+              {nav.name}
+            </a>
           ))}
         </div>
-      </DisclosurePanel>
-    </Disclosure>
-  )
-}
+        {/* language menu */}
+        <Menu as="div" className="relative ml-3">
+          <MenuButton className="px-4 py-6 text-lg font-medium rounded-lg text-white hover:bg-green-800 hover:text-secondary focus:outline-none focus:ring-inset">
+          <img src={langs[currentLang].flag} alt={`${currentLang} flag`} className="h-12 w-12 mx-4" />          </MenuButton>
+          <MenuItems className="bg-gradient-to-br from-green-500 via-green-600 to-green-600 absolute right-0 w-48 mt-2 origin-top-right divide-y divide-gray-100 shadow-lg focus:outline-none shadow-xl">
+            {Object.keys(langs).map((lang) => (
+              <MenuItem key={lang}>
+                {({ active }) => (
+                  <button
+                    onClick={() => handleLangChange(lang)}
+                    className={`w-full flex px-4 py-2 text-sm text-white ${active ? 'text-secondary bg-green-300' : ''}`}                  >
+                      <img src={langs[lang].flag} alt={`${lang} flag`} className="h-8 w-8 mx-4" />
+                    {langs[lang].nativeName}
+                  </button>
+                )}
+              </MenuItem>
+            ))}
+          </MenuItems>
+        </Menu>
+        {/* hamburger menu for mobile view */}
+        <Menu as="div" className="relative md:hidden">
+          <MenuButton className="px-4 py-6 text-lg font-medium rounded-lg text-white hover:bg-green-800 hover:text-yellow-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <Bars3Icon className="h-8 w-8" />
+          </MenuButton>
+          <MenuItems className="bg-gradient-to-br from-green-500 via-green-600 to-green-600 absolute right-0 w-48 mt-2 origin-top-right divide-y divide-gray-100 shadow-lg focus:outline-none shadow-xl">
+            {navigation.map((nav) => (
+              <MenuItem key={nav.key}>
+                {({ active }) => (
+                  <a
+                    href={nav.href}
+                    className={`block px-4 py-2 text-sm text-white ${active ? 'text-secondary' : ''}`}
+                  >
+                    {nav.name}
+                  </a>
+                )}
+              </MenuItem>
+            ))}
+          </MenuItems>
+        </Menu>
+
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
