6fa6dbc6f0cdea9c1e40c2237af0d903edd10fc3
karl komeya <karlkomeya04@gmail.com>
2024-09-16 19:35:31 +0900

    chore:updated UI for volunteer page

--- src/pages/VolunteerPage.tsx
+++ src/pages/VolunteerPage.tsx
@@ -6,17 +6,17 @@ const VolunteerPage = () => {
   const { t } = useTranslation()
     
   return (
-    <div className="flex flex-col md:flex-row h-screen w-screen">
+    <div className="flex flex-col md:flex-row h-screen w-screen overflow-hidden ">
         <div className="(Image) bg-volunteer-background w-full md:w-1/2 h-1/2 md:h-full bg-cover 
         bg-center flex items-center justify-center">
         </div>
-        <div className="w-full md:w-1/2 py-6 px-4 md:px-16 bg-gradient-to-br from-green-300
+        <div className=" md:w-1/2 py-6 px-4 md:px-16 bg-gradient-to-br from-green-300
          to-white font-sans flex items-center justify-center">
             <div className="w-full">
-         <div className="mb-20 text-center text-black text-2xl md:text-4xl lg:text-6xl p-4 md:p-0">
+         <div className="mb-20 text-center text-black text-2xl md:text-4xl lg:text-2xl lg:mt-10 p-4 md:p-0">
              {t("volunteer.slug")}
+                <div className='xl:mt-5 mt-2 lg:h-1/2'><VolunteerForm /></div>
          </div>
-                <VolunteerForm />
             </div>
         </div>
     </div>
