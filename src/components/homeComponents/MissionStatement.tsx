80291fb9253355855b2df6e18d9c3f8eabf99aeb
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-12-10 10:33:59 +0900

    Feat: added animations for text.

--- src/components/homeComponents/MissionStatement.tsx
+++ src/components/homeComponents/MissionStatement.tsx
@@ -1,19 +1,23 @@
-import { useTranslation } from 'react-i18next';
-export default function MissionStatement() {
+import { useTranslation } from "react-i18next";
 
-   const { t } = useTranslation();
-   
-   return (
-<div>
-   <div className="flex flex-col portrait:gap-3 gap-10 flex-1 items-center justify-center  md:py-16">
-      <h1 className="text-brown text-3xl p-4 portrait:text-xl lg:text-3xl xl:text-5xl max-w-[1200px] mx-auto w-full text-center font-semibold">{t("main.vision")}</h1>
-      <h2 className="text-brown text-xl lg:text-lg portrait:text-lg xl:text-3xl max-w-[1200px]  w-full text-center font-semibold">{t("main.category")}</h2>
-      <ul className="text-brown text-lg p-4 lg:text-xl portrait:text-sm xl:text-2xl max-w-[1200px] mx-auto w-full text-center font-semibold">
-         <li>{t("main.list1")}</li>
-         <li>{t("main.list2")}</li>
-         <li>{t("main.list3")}</li>
-         </ul>
+export default function MissionStatement() {
+ const { t } = useTranslation();
+
+ return (
+  <div>
+   <div className="flex flex-col portrait:gap-3 gap-10 flex-1 items-center justify-center md:py-16">
+    <h1 className="text-brown text-3xl p-4 portrait:text-xl lg:text-3xl xl:text-5xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+     {t("main.vision")}
+    </h1>
+    <h2 className="text-brown text-xl lg:text-lg portrait:text-lg xl:text-3xl max-w-[1200px]  w-full text-center font-semibold">
+     {t("main.category")}
+    </h2>
+    <ul className="text-brown text-lg p-4 lg:text-xl portrait:text-sm xl:text-2xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+     <li>{t("main.list1")}</li>
+     <li>{t("main.list2")}</li>
+     <li>{t("main.list3")}</li>
+    </ul>
    </div>
-</div>
-   );
+  </div>
+ );
 }
